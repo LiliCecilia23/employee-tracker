a612d62afae6b51f659058e33ee2feae40cc1332
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-07 11:05:50 -0500

    created tables in sql file

---
+++ Develop/tracker.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department(
+    id INT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
