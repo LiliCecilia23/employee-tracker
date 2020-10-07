3b9dbee9af7b31667d9868dec26204e80cc65011
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-07 11:55:03 -0500

    add department function

--- Develop/tracker.js
+++ Develop/tracker.js
@@ -28,9 +28,55 @@ inquirer
 ]).then(answer => {
     if (answer.name === "Add department, role, or employee"){
         // Create function
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "Would you like to add a department, role, or employee?",
+                choices: ["Department", "Role", "Employee"],
+                name: "addChoice"
+            }
+        ]).then(answer => {
+            if (answer.name === "Department"){
+                addDept();
+            } else if (answer.name === "Role"){
+
+            } else if (answer.name === "Employee"){
+
+            }
+        })
     } else if (answer.name === "View departments, roles, or employees"){
         // Read function
     } else if (answer.name === "Update employee roles"){
         // Update function
     }
-})
+});
+
+function addDept() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of this department?",
+            name: "deptNAME"
+        },
+        {
+            type: "input",
+            message: "What is this department's ID?",
+            name: "deptID"
+        }
+    ]).then(answer => {
+        const {deptName, deptID} = answer;
+
+        let query = connection.query(
+            "INSERT INTO department SET ?",
+            {
+                id: deptID,
+                name: deptNAME
+            }, function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows + " department added!\n");
+                //can chain another function here if I want
+            }
+        );
+    });
+};
