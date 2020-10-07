290d2b6aab4dd54ba6616dd8fb48c845cd195dea
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-07 14:47:40 -0500

    added role and department data

---
+++ Develop/sql/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"), (2, "Accounting"), (3, "Customer Service"), (4, "Quality Assurance"), (5, "Other");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (11, "Sales Staff", 30000.00, 1), (12, "Accountant", 40000.00, 2),
+    (13, "CS Rep", 35000.00, 3), (14, "QA Rep", 35000.000, 4),
+    (15, "Receptionist", 25000.00, 5), (16, "Intern", 20000.00, 5),
+    (17, "Branch Manager", 45000.00, 5), (18, "HR rep", 35000.00, 5);
--- Develop/sql/tracker.sql
+++ Develop/sql/tracker.sql
@@ -4,25 +4,25 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department(
-    id INT,
+    id INT(10),
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INT,
+    id INT(10),
     title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
+    salary DECIMAL(10, 4),
+    department_id INT(10),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
-    id INT,
+    id INT(10),
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
+    role_id INT(10),
+    manager_id INT(10),
     PRIMARY KEY (id)
 );
 
