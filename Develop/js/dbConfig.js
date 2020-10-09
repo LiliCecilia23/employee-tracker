89b81bd4d314c0688aa90df77e493f3f908863cd
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-09 15:03:48 -0500

    dbConfig edit

--- Develop/js/dbConfig.js
+++ Develop/js/dbConfig.js
@@ -4,7 +4,7 @@ let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: "Sriracha96$",
+    password: "",
     database: 'tracker_db'
 });
 
