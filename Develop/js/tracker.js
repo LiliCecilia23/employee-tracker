704a003640587af2fe1f17d7e764e4ea2db308bc
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-13 15:11:04 -0500

    moved everything from dept file into tracker

--- Develop/js/tracker.js
+++ Develop/js/tracker.js
@@ -122,4 +122,58 @@ function updateRole(){
     });
 };
 
-module.exports = {startTracker, adder, reader, updateRole};
+// DEPARTMENT FUNCTIONS
+function addDept() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of this department?",
+            name: "deptNAME"
+        },
+        {
+            type: "input",
+            message: "What is this department's ID?",
+            name: "deptID"
+        }
+    ]).then(answer => {
+        const {deptNAME, deptID} = answer;
+
+        let query = connection.query(
+            "INSERT INTO department SET ?",
+            {
+                id: deptID,
+                name: deptNAME
+            }, function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows + " department added!\n");
+                //can chain another function here if I want
+            }
+        );
+
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to add anything else?",
+                choices: ["Yes", "No (return to home screen)"],
+                name: "rerunChoice"
+            }
+        ]).then(answer => {
+            if (answer.rerunChoice === "Yes"){
+                adder();
+            } else {
+                startTracker();
+            }
+        })
+    });
+};
+
+function readDept(){
+    let query = connection.query(
+        "SELECT * FROM department",
+        function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        }
+    );
+};
